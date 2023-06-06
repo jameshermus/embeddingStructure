7db218924c65f514542c7e4354f73695c3caf27c
jameshermus <jameshermus@gmail.com>
2023-06-06 11:32:21 +0200

    test writing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # embeddingStructure
+
+testwrite
--- main.py
+++ main.py
@@ -9,13 +9,14 @@ from stable_baselines3.common.vec_env import VecFrameStack # used to vectorize e
 from stable_baselines3.common.vec_env import VecNormalize
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
 import matplotlib.pyplot as plt
 
 # http://localhost:6006/
 
 # computationType = 'EvaluatePreLearning'
-# computationType = 'Learn'
-computationType = 'hardcode'
+computationType = 'Learn'
+# computationType = 'hardcode'
 # computationType = 'Learn - Vectorized'
 # computationType = 'Evaluate'
 
@@ -36,7 +37,14 @@ if( computationType == 'EvaluatePreLearning'):
     env.close()
 
 if( computationType == 'hardcode'):
-# Hard code submovement as a sanity check
+
+    # Add code to import a model
+    saveName = 'PPO_Submoement_LearnVec20_000'
+    log_path = os.path.join('Training','Logs')
+    iiwaTest_path = os.path.join('Training','Saved_Models', saveName)
+    model = PPO.load(iiwaTest_path)
+
+    # Hard code submovement as a sanity check
     env = PyBulletRobot(renderType = True)
     obs = env.reset()
     done = False
@@ -47,11 +55,15 @@ if( computationType == 'hardcode'):
     x0 = np.zeros((3,int(N)))
     x0_dot = np.zeros((3,int(N)))
     while not done:
-        if( count == 100 ):
-            # action = env.action_space.sample()
-            action = np.array([[0.9],[0.2],[0.1],[np.pi*(0.0)]])# Apply submovement - Place selection greater than 0.5
-        else:
-            action = np.array([[0.1],[10.0],[10.0],[10.0]]) # No submovement - Place selection less than 0.5
+
+        # if( count == 100 ):
+        #     # action = env.action_space.sample()
+        #     action = np.array([[0.9],[0.2],[0.1],[np.pi*(0.0)]])# Apply submovement - Place selection greater than 0.5
+        # else:
+        #     action = np.array([[0.1],[10.0],[10.0],[10.0]]) # No submovement - Place selection less than 0.5
+        
+        action = model.predict(obs,deterministic=False)[0] # Use trained model
+
         obs, reward, done, info = env.step(action)
         score += reward
         x_0b_b, x0_dot_b = env.robot.sumOnGoingSubmovements(env.time)
@@ -97,16 +109,10 @@ if(computationType ==  'Learn'):
     # - check tensorboard learn how modify
     # - add call backs
     # - Try to implament as an integration of velocity
-
-    # stop_callback = StopTrainingOnRewardThreshold(reward_threshold=-100, verbose=1)
-    # eval_callback = EvalCallback(env, 
-    #                              callback_on_new_best=stop_callback, 
-    #                              eval_freq=10_000, 
-    #                              best_model_save_path=save_path, 
-    #                              verbose=1)
-
+    
+    saveName = 'PPO_Submoement_Learn'
     log_path = os.path.join('Training','Logs')
-    iiwaTest_path = os.path.join('Training','Saved_Models', 'PPO_Submoement_Learn20_000')
+    save_path = os.path.join('Training','Saved_Models', saveName)
 
     # Training
     env = PyBulletRobot(renderType = False)
@@ -114,9 +120,16 @@ if(computationType ==  'Learn'):
 
     model = PPO('MlpPolicy',env,verbose=1,tensorboard_log=log_path)
 
-    model.learn(total_timesteps=20_000) #,callback=eval_callback)
+    stop_callback = StopTrainingOnRewardThreshold(reward_threshold=-20, verbose=1)
+    eval_callback = EvalCallback(env, 
+                                 callback_on_new_best=stop_callback, 
+                                 eval_freq=20_000, 
+                                 best_model_save_path=save_path, 
+                                 verbose=1)
 
-    model.save(iiwaTest_path)
+    model.learn(total_timesteps=100_000_000,callback=eval_callback)
+
+    model.save(save_path)
 
     evaluate_policy(model,env, n_eval_episodes=10,render=False)
 
@@ -146,78 +159,79 @@ if(computationType ==  'Learn - Vectorized'):
 
     evaluate_policy(model,vec_env, n_eval_episodes=10,render=False)
 
-if ( computationType == 'Evaluate'):
-    env_render = PyBulletRobot(renderType = False)
+if (computationType == 'Evaluate'):
+    saveName = 'PPO_Submoement_Learn20_000'
+
+    env_render = PyBulletRobot(renderType = True)
     log_path = os.path.join('Training','Logs')
 
     iiwaTest_path = os.path.join('Training','Saved_Models', saveName)
 
     model = PPO.load(iiwaTest_path)
 
-    evaluate_policy(model,env_render, n_eval_episodes=10,render=False)
-
+    # evaluate_policy(model,env_render, n_eval_episodes=10,render=False)
 
     # Look at it trained model
     episodes = 1
-    xFinal = np.zeros((3,episodes))
-    xStart = np.zeros((3,episodes))
-    aFinal = np.zeros((3,episodes))
-    distList = np.linspace(0.1,1,num=episodes)
-    reward = np.zeros(episodes)
-    actionAll = []
+    # xFinal = np.zeros((3,episodes))
+    # xStart = np.zeros((3,episodes))
+    # aFinal = np.zeros((3,episodes))
+    # distList = np.linspace(0.1,1,num=episodes)
+    # reward = np.zeros(episodes)
+    # actionAll = []
 
     for episode in range(1,episodes+1):
         obs = env_render.reset()
-        xStart[0:3,episode-1] = obs[0:3,0]
+        # xStart[0:3,episode-1] = obs[0:3,0]
         done = False
         score = 0
         while not done:
             # action = env_render.action_space.sample() # Sample action space
-            action = model.predict(obs,deterministic=False)[0]            # Use trained model
+            action = model.predict(obs,deterministic=False)[0] # Use trained model
             # action = np.array([0.3,distList[episode-1],0.3]) # Hard code
-            obs, reward[episode-1], done, info = env_render.step(action)
+            obs, reward, done, info = env_render.step(action)
         
         # aFinal[0:3,episode-1] = action
-        xFinal[0:3,episode-1] = obs[0:3,0]
+        # xFinal[0:3,episode-1] = obs[0:3,0]
         # actionAll.append(action)
             
     env_render.close()
 
-    bestIndex = np.argmax(reward)
+    # bestIndex = np.argmax(reward)
 
-    # X-Y position of robot
-    target = np.array([[0.77982756],[0.2],[0.27314214]])
-    fig, ax = plt.subplots()
+    # # X-Y position of robot
+    # target = np.array([[0.77982756],[0.2],[0.27314214]])
+    # fig, ax = plt.subplots()
 
-    for i in range(0,episodes):
-        # ax.scatter(xStart[i][0],xStart[i][1],xStart[i][2], marker='o',markersize=50)
-        ax.plot(xFinal[0,i],xFinal[1,i],marker='o',markersize=10,color='r')
-        ax.plot(xStart[0,i],xStart[1,i],marker='o',markersize=10,color='b')
-        # print(i)
-        # input('Press <ENTER> to continue')
+    # for i in range(0,episodes):
+    #     # ax.scatter(xStart[i][0],xStart[i][1],xStart[i][2], marker='o',markersize=50)
+    #     ax.plot(xFinal[0,i],xFinal[1,i],marker='o',markersize=10,color='r')
+    #     ax.plot(xStart[0,i],xStart[1,i],marker='o',markersize=10,color='b')
+    #     # print(i)
+    #     # input('Press <ENTER> to continue')
 
-    ax.plot(xFinal[0,bestIndex],xFinal[1,bestIndex],marker='o',markersize=10,color='y')
-    ax.plot(target[0],target[1],marker='o',markersize=10,color='k')
+    # ax.plot(xFinal[0,bestIndex],xFinal[1,bestIndex],marker='o',markersize=10,color='y')
+    # ax.plot(target[0],target[1],marker='o',markersize=10,color='k')
 
-    plt.xlim(-1, 1)
-    plt.ylim(-1, 1)
-    plt.show()
+    # plt.xlim(-1, 1)
+    # plt.ylim(-1, 1)
+    # plt.show()
 
-    fig, ax = plt.subplots()
-    ax.plot(reward)
+    # fig, ax = plt.subplots()
+    # ax.plot(reward)
 
-    # Histogram of X,Y,Z
-    names_xyz = ['x','y','z']
-    fig, ax2 = plt.subplots(3)
-    for i in range(0,3): 
-        x = xFinal[i,:]
-        counts, bins = np.histogram(x)
-        ax2[i].stairs(counts,bins)
-        countsMax = np.max(counts)
-        ax2[i].plot([target[i],target[i]],[0,countsMax],color='k')
-        ax2[i].set_xlim(0,1)
-        ax2[i].set_title(names_xyz[i])
-    plt.show()
+    # # Histogram of X,Y,Z
+    # names_xyz = ['x','y','z']
+    # fig, ax2 = plt.subplots(3)
+    # for i in range(0,3): 
+    #     x = xFinal[i,:]
+    #     counts, bins = np.histogram(x)
+    #     ax2[i].stairs(counts,bins)
+    #     countsMax = np.max(counts)
+    #     ax2[i].plot([target[i],target[i]],[0,countsMax],color='k')
+    #     ax2[i].set_xlim(0,1)
+    #     ax2[i].set_title(names_xyz[i])
+    # plt.show()
 
 
     print('end of code')
